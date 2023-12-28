2e4159fbb159d238dc194e54829ecb772bab76b4
Harshchauhan29626 <150172292+Harshchauhan29626@users.noreply.github.com>
2023-12-28 13:41:57 +0530

    Add files via upload

---
+++ Set input value/script.js
@@ -0,0 +1,8 @@
+function change() {
+
+    let name = document.getElementById("name").value;
+    let messag = document.getElementById("message").value;
+
+    document.getElementById("p1").innerHTML = name + " : " + messag ;
+
+}
